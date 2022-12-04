81c6823582efca30d44497091d153b3bb11e6dd2
V1TSO <v.renasco.larrain@gmail.com>
2022-12-04 16:36:24 -0300

    Fixed bottom not showing on small displays on ABOUT PAGE

--- src/components/About.js
+++ src/components/About.js
@@ -13,7 +13,7 @@ const About = () => {
             <Typography variant="h5" color="text.primary" marginBottom='3rem'>Quizzler is a web app about quizzes that lets you learn more about yourself and test your knowledge. All the responses are made using AI.</Typography>
             <Typography variant="h4" color="text.primary" marginBottom='1rem' fontWeight='bold'>VR:</Typography>
             <Typography fontStyle='italic' variant="h5" color="text.primary" marginBottom='3rem'>"This is a project that I made to learn more about API's, Node.js, React and mui styling"</Typography>
-            <Stack direction="row" spacing={2} sx={{ bottom: "5rem", position: 'fixed' }}>
+            <Stack direction="row" spacing={2} sx={{ bottom: "5rem", position: 'relative', bottom: 0 }}>
                 <Avatar
                     alt="Vicente Renasco"
                     src="https://images.unsplash.com/photo-1436891620584-47fd0e565afb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80"
