861eece22ca7f760787bc35352390adbbead040a
V1TSO <v.renasco.larrain@gmail.com>
2022-12-04 01:49:06 -0300

    Changed button and a tag href

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -6,8 +6,8 @@ const Landing = () => {
             <div className='landing-card'>
                 <h1>Quizzler</h1>
                 <h2>Test yourself with these fun quizes!</h2>
-                <a href="/#/1">
-                    <button href="/#/1">Start Now</button>
+                <a href="#/1">
+                    <button href="#/1">Start Now</button>
                 </a>
             </div>
         </div>
